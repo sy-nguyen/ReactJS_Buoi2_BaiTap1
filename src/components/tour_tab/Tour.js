a393abb776bc68555bc96949d51992063e05ed96
sy-nguyen <syvipko42@gmail.com>
2024-07-23 08:55:49 +0700

    update 2

--- src/components/tour_tab/Tour.js
+++ src/components/tour_tab/Tour.js
@@ -1,21 +1,34 @@
 import { Card, CardBody, CardSubtitle, CardTitle, Col, Row } from "reactstrap";
+import "./tour.css"
 
 
 export default function Tour(props) {
-  const { pro} = props;
+  const { pro } = props;
   return (
     <Col lg={3} md={6} xs={12} className="mb-4">
-        <Card>
-          <div>
-            <img src={pro.img}/>
-            <p>12 off</p>
+      <Card className="card">
+        <img className="card-img" src={pro.img} />
+        <p className="button-img">12 off</p>
+        <CardBody>
+          <CardSubtitle className="card-city">
+            <i class="fa-solid fa-map-pin"></i>
+            {pro.city}
+            </CardSubtitle>
+          <h5 className="card-name">{pro.name}</h5>
+          <p className="card-text">
+            From 
+            <span className="card-prices">${pro.prices}</span>
+            <span className="card-prices-line">$250.00</span>
+          </p>
+          <div className="card-star">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-regular fa-star-half-stroke"></i>
           </div>
-          <CardBody>
-            <CardSubtitle>{pro.city}</CardSubtitle>
-            <CardTitle>{pro.name}</CardTitle>
-            <p>{pro.prices}</p>
-          </CardBody>
-          </Card>
+        </CardBody>
+      </Card>
     </Col>
   );
 }
--- src/components/tour_tab/Tours.js
+++ src/components/tour_tab/Tours.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Tour from './Tour';
 import img1 from "./imgs_tour/tour-1.png"
 import img2 from "./imgs_tour/tour-2.png"
@@ -19,24 +19,24 @@ import img16 from "./imgs_tour/tour-16.png"
 import { Container, Row } from 'reactstrap';
 import "./tours.css"
 
-export default function Tours(){
-    const [list, setList]= useState([
-        {id:1, city:"New York", name:"Three Temples Bangkok City Tour Must Visit", prices:149.99, img:img1},
-        {id:2, city:"New York", name:"Essence of Vietnam South to North", prices:167.19, img:img2},
-        {id:3, city:"New York", name:"Osa Peninsula to Dominical City Tour", prices:134.99, img:img3},
-        {id:4, city:"New York", name:"Mt Fuji, Hakone, Lake Ashi Cruise", prices:50.15, img:img4},
-        {id:5, city:"London", name:"Three Temples Bangkok City Tour Must Visit", prices:149.99,img:img5},
-        {id:6, city:"London", name:"Essence of Vietnam South to North", prices:167.19,img:img6},
-        {id:7, city:"London", name:"Osa Peninsula to Dominical City Tour", prices:134.99,img:img7},
-        {id:8, city:"London", name:"Mt Fuji, Hakone, Lake Ashi Cruise", prices:50.15,img:img8},
-        {id:9, city:"Tokyo", name:"Three Temples Bangkok City Tour Must Visit", prices:149.99,img:img9},
-        {id:10, city:"Tokyo", name:"Essence of Vietnam South to North", prices:167.19,img:img10},
-        {id:11, city:"Tokyo", name:"Osa Peninsula to Dominical City Tour", prices:134.99,img:img11},
-        {id:12, city:"Tokyo", name:"Mt Fuji, Hakone, Lake Ashi Cruise", prices:50.15,img:img12},
-        {id:13, city:"Los Angeles", name:"Three Temples Bangkok City Tour Must Visit", prices:149.99,img:img13},
-        {id:14, city:"Los Angeles", name:"Essence of Vietnam South to North", prices:167.19,img:img14},
-        {id:15, city:"Los Angeles", name:"Osa Peninsula to Dominical City Tour", prices:134.99,img:img15},
-        {id:16, city:"Los Angeles", name:"Mt Fuji, Hakone, Lake Ashi Cruise", prices:50.15,img:img16},
+export default function Tours() {
+    const [list, setList] = useState([
+        { id: 1, city: "New York", name: "Three Temples Bangkok City Tour Must Visit", prices: 149.99, img: img1 },
+        { id: 2, city: "New York", name: "Essence of Vietnam South to North", prices: 167.19, img: img2 },
+        { id: 3, city: "New York", name: "Osa Peninsula to Dominical City Tour", prices: 134.99, img: img3 },
+        { id: 4, city: "New York", name: "Mt Fuji, Hakone, Lake Ashi Cruise", prices: 50.15, img: img4 },
+        { id: 5, city: "London", name: "Three Temples Bangkok City Tour Must Visit", prices: 149.99, img: img5 },
+        { id: 6, city: "London", name: "Essence of Vietnam South to North", prices: 167.19, img: img6 },
+        { id: 7, city: "London", name: "Osa Peninsula to Dominical City Tour", prices: 134.99, img: img7 },
+        { id: 8, city: "London", name: "Mt Fuji, Hakone, Lake Ashi Cruise", prices: 50.15, img: img8 },
+        { id: 9, city: "Tokyo", name: "Three Temples Bangkok City Tour Must Visit", prices: 149.99, img: img9 },
+        { id: 10, city: "Tokyo", name: "Essence of Vietnam South to North", prices: 167.19, img: img10 },
+        { id: 11, city: "Tokyo", name: "Osa Peninsula to Dominical City Tour", prices: 134.99, img: img11 },
+        { id: 12, city: "Tokyo", name: "Mt Fuji, Hakone, Lake Ashi Cruise", prices: 50.15, img: img12 },
+        { id: 13, city: "Los Angeles", name: "Three Temples Bangkok City Tour Must Visit", prices: 149.99, img: img13 },
+        { id: 14, city: "Los Angeles", name: "Essence of Vietnam South to North", prices: 167.19, img: img14 },
+        { id: 15, city: "Los Angeles", name: "Osa Peninsula to Dominical City Tour", prices: 134.99, img: img15 },
+        { id: 16, city: "Los Angeles", name: "Mt Fuji, Hakone, Lake Ashi Cruise", prices: 50.15, img: img16 },
     ])
 
     const [filteredList, setFilteredList] = useState([]);
@@ -51,36 +51,53 @@ export default function Tours(){
         setSelectedCity(city);
     };
 
-    return(
+    return (
         <Container className="tours">
             <div className="header_tours">
                 <h3>Perfect destination</h3>
                 <h2>Trending destinations</h2>
             </div>
-            <ul className='tab_list'>
-                <li className='item'>
-                    <button onClick={() => handleCityClick("New York")}>New York</button>
+            <ul className="tab_list">
+                <li className="item">
+                    <button
+                        className={selectedCity === 'New York' ? 'active' : ''}
+                        onClick={() => handleCityClick('New York')}
+                    >
+                        New York
+                    </button>
                 </li>
-                <li className='item'>
-                    <button onClick={() => handleCityClick("London")}>London</button>
+                <li className="item">
+                    <button
+                        className={selectedCity === 'London' ? 'active' : ''}
+                        onClick={() => handleCityClick('London')}
+                    >
+                        London
+                    </button>
                 </li>
-                <li className='item'>
-                    <button onClick={() => handleCityClick("Tokyo")}>Tokyo</button>
+                <li className="item">
+                    <button
+                        className={selectedCity === 'Tokyo' ? 'active' : ''}
+                        onClick={() => handleCityClick('Tokyo')}
+                    >
+                        Tokyo
+                    </button>
                 </li>
-                <li className='item'>
-                    <button onClick={() => handleCityClick("Los Angeles")}>Los Angeles</button>
+                <li className="item">
+                    <button
+                        className={selectedCity === 'Los Angeles' ? 'active' : ''}
+                        onClick={() => handleCityClick('Los Angeles')}
+                    >
+                        Los Angeles
+                    </button>
                 </li>
             </ul>
             <div>
                 <Row>
-                    {
-                        filteredList.map((item, index) => (
-                            <Tour key={index} pro={item} />
-                        ))
-                    }
+                    {filteredList.map((item, index) => (
+                        <Tour key={index} pro={item} />
+                    ))}
                 </Row>
-            </div> 
+            </div>
         </Container>
-    )
-
+    );
 }
